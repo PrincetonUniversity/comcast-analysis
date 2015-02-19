bugfixes: change back to counter fix plotting scripts to work with counters and lists use cumsum method to make cdfs calculate peakBytesPerDevice in calculation loop instead of csv loop

plotter.py
@@ -26,8 +26,8 @@ head -300 control1.dat > test-control-set.dat
 tail -300 control1.dat >> test-control-set.dat
 '''
 
-DATASET='250-test'
-CONTROLSET='control1'
+DATASET='test-data-set'#'250-test'
+CONTROLSET='test-control-set'#'control1'
 
 keys = ['Device_number',
          'end_time',
@@ -56,17 +56,33 @@ def read_dataframe(folder, filename):
     header_row = ['device_number', 'end_time', 'cmts_inet', 'direction', 'bytes']
     return pd.read_csv(PROCESSEDPATH + folder + '/' + filename, names=header_row)
 
-
 # CDF plotter
-def plotCDF(data, ax, c='blue'):
+def plotCDF_list(data, ax, c='blue'):
     '''
     data = list for which we need to plot cdf
     ax = axis
     '''
     sorted_data=np.sort( data )
-    yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    #logger.debug(sorted_data)
+    #yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    yvals=np.cumsum(sorted_data)
+    #logger.debug(yvals)
     ax.plot( sorted_data, yvals, color=c )
     return
+
+def plotCDF_counter(counter, ax, c='blue'):
+    '''
+    data = Counter object, not in increasing order
+    ax = axis
+    '''
+    sorted_keys=sorted( counter )
+    #log.debug(sorted_data)
+    #yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    yvals=np.cumsum([counter[k] for k in sorted_keys])
+    #log.debug(yvals)
+    ax.plot( sorted_keys, yvals, color=c, marker='o' )
+    return
+
 
 if __name__ == "__main__":
 
@@ -82,35 +98,44 @@ if __name__ == "__main__":
     logger.info("Plotting...")
     # plot double subplot CDFs for upstream and downstream
     # peak usage per user over all time
-    fig0, ax0 = plt.subplots(2, 1)
+    fig0, ax0 = plt.subplots(2, 1, sharex=True)
 
-    plotCDF(peakUsage['up'], ax0[0])
-    plotCDF(peakUsage_control['up'], ax0[0], 'g')
+    plotCDF_list(peakUsage['up'].values(), ax0[0])
+    plotCDF_list(peakUsage_control['up'].values(), ax0[0], 'g')
 
-    plotCDF(peakUsage['dw'], ax0[1])
-    plotCDF(peakUsage_control['dw'], ax0[1], 'g')
+    plotCDF_list(peakUsage['dw'].values(), ax0[1])
+    plotCDF_list(peakUsage_control['dw'].values(), ax0[1], 'g')
 
-    fig1.savefig(OUTPUTPATH + "peakBytesPerUser")
+    ax0[1].set_xlabel('Peak Bytes per Device Number [Bytes]')
+    ax0[0].set_ylabel('CDF')
+    ax0[1].set_ylabel('CDF')
+    fig0.savefig(OUTPUTPATH + "peakBytesPerDeviceCDF")
 
     # No Zeros CDF
     fig1, ax1 = plt.subplots(2, 1)
 
-    plotCDF(noZeroHist['up'], ax1[0])
-    plotCDF(noZeroHist_control['up'], ax1[0], 'g')
+    plotCDF_counter(noZeroHist['up'], ax1[0])
+    plotCDF_counter(noZeroHist_control['up'], ax1[0], 'g')
 
-    plotCDF(noZeroHist['dw'], ax1[1])
-    plotCDF(noZeroHist_control['dw'], ax1[1], 'g')
+    plotCDF_counter(noZeroHist['dw'], ax1[1])
+    plotCDF_counter(noZeroHist_control['dw'], ax1[1], 'g')
 
+    ax1[1].set_xlabel('Non Zero Bytes per Device time-slot [Bytes]')
+    ax1[0].set_ylabel('CDF')
+    ax1[1].set_ylabel('CDF')
     fig1.savefig(OUTPUTPATH + "noZeroCDF")
     #fig1.show()
 
     fig2, ax2 = plt.subplots(2, 1)
 
-    plotCDF(peakHist['up'], ax2[0])
-    plotCDF(peakHist_control['up'], ax2[0], 'g')
+    plotCDF_counter(peakHist['up'], ax2[0])
+    plotCDF_counter(peakHist_control['up'], ax2[0], 'g')
 
-    plotCDF(peakHist['dw'], ax2[1])
-    plotCDF(peakHist_control['dw'], ax2[1], 'g')
+    plotCDF_counter(peakHist['dw'], ax2[1])
+    plotCDF_counter(peakHist_control['dw'], ax2[1], 'g')
 
-    fig2.savefig(OUTPUTPATH + "peakCDF")
+    ax2[1].set_xlabel('Peak Bytes per Device per Day [Bytes]')
+    ax2[0].set_ylabel('CDF')
+    ax2[1].set_ylabel('CDF')
+    fig2.savefig(OUTPUTPATH + "peakBytesPerDayPerDeviceCDF")
     #fig2.show()
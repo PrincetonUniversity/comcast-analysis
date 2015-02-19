63d239b7c00b695a97184a4625886914e2cd63e3
Sarthak <shahifaqeer@gmail.com>
2015-02-19 16:34:12 +0000

    bugfixes: change back to counter fix plotting scripts to work with counters and lists use cumsum method to make cdfs calculate peakBytesPerDevice in calculation loop instead of csv loop

--- analysis.py
+++ analysis.py
@@ -22,8 +22,8 @@ head -300 control1.dat > test-control-set.dat
 tail -300 control1.dat >> test-control-set.dat
 '''
 
-DATASET='250-test'
-CONTROLSET='control1'
+DATASET='test-data-set' #'250-test'
+CONTROLSET='test-control-set'#'control1'
 
 keys = ['Device_number',
          'end_time',
@@ -44,8 +44,8 @@ header_row = ['Device_number',
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger()
 logger.setLevel('DEBUG')
-DEBUG_ROW_DISPLAY = 100000      #display every 100,000th row in original data
-DEBUG_LOG_COUNTER_DISPLAY = 100     #display every 100th device number
+DEBUG_ROW_DISPLAY = 100 #100000      #display every 100,000th row in original data
+DEBUG_LOG_COUNTER_DISPLAY = 1 #100     #display every 100th device number
 
 # DATA SPLITTING
 
@@ -54,7 +54,6 @@ def dat2csv_by_deviceNum(datasetname):
     separate huge csv into multiple deviceNumber.csv
     also store peakBytesPerUser
     """
-    peakBytesPerUser = {'up':defaultdict(int), 'dw':defaultdict(int)}
     folder = datasetname.split('.')[0]
     if not os.path.exists(PROCESSEDPATH + folder):
         os.makedirs(PROCESSEDPATH + folder)
@@ -62,8 +61,6 @@ def dat2csv_by_deviceNum(datasetname):
     with open(DATAPATH + datasetname, 'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter='|')
         count = 0
-        # store peak bytes per user
-        peak = 0
         for row in csvreader:
             deviceNum = row[0]
             # append data to avoid loading huge arrays
@@ -73,31 +70,16 @@ def dat2csv_by_deviceNum(datasetname):
             # Device_number, end_time, cmts_inet, service_direction,
             # octets_passed
             writer.writerow( (row[0], row[1], row[4], row[5], row[7]) )
-            # get max of the file upstream or downstream
-            if int(row[5])== 2:
-                peakBytesPerUser['up'][deviceNum] = max(peakBytesPerUser[deviceNum],
-                                                        int(row[7]))
-            else:
-                peakBytesPerUser['dw'][deviceNum] = max(peakBytesPerUser[deviceNum],
-                                                        int(row[7]))
             f.close()
             #print every 100,000th row in the dataset
             count += 1
             if count % DEBUG_ROW_DISPLAY == 0:
                 logger.debug(datasetname + ' ' + row[0] + ' ' + row[1])
         logger.info("Done converting to individual csv")
-    return peakBytesPerUser
+    return
 
 def create_csv(datasetname):
-    peakBytesPerUser = dat2csv_by_deviceNum(datasetname)
-    # save peakBytes per User pkl
-    folder = datasetname.split('.')[0]
-    if not os.path.exists(OUTPUTPATH+folder):
-        os.makedirs(OUTPUTPATH+folder+"/")
-    f = open(OUTPUTPATH+folder+"/"+'peakBytesPerUser.pkl', 'w')
-    pkl.dump(peakBytesPerUser, f)
-    f.close()
-    logger.info("Pickled peak bytes per user for " + folder)
+    dat2csv_by_deviceNum(datasetname)
     return
 
 def read_dataframe(folder, filename):
@@ -108,8 +90,9 @@ def read_dataframe(folder, filename):
 # Calculate non zero usage per day for each device num
 
 # GLOBAL lists to get CDFs
-noZeroHist = {'up':[], 'dw':[]}
-peakHist = {'up':[], 'dw':[]}
+noZeroHist = {'up':Counter([]), 'dw':Counter([])}
+peakHist = {'up':Counter([]), 'dw':Counter([])}
+peakBytesPerUser = {'up':defaultdict(int), 'dw':defaultdict(int)}
 
 def update_cdf(filename, folder):
 
@@ -118,11 +101,18 @@ def update_cdf(filename, folder):
     df_dev['date'] = df_dev['end_time'].apply(lambda x: x.split(" ")[0])
     df_dev['time'] = df_dev['end_time'].apply(lambda x: x.split(" ")[1])
 
-    # 1. Peak Per Day CDF
     # split into dw (1) and up (2)
     df_dw = df_dev[df_dev['service_direction'] == 1]
     df_up = df_dev[df_dev['service_direction'] == 2]
 
+    # 0. Peak bytes per day per user
+    # get max of the file upstream or downstream
+    deviceNum = int(filename.rstrip('.csv'))
+    peakBytesPerUser['up'][deviceNum] = df_up['octets_passed'].max()
+    peakBytesPerUser['dw'][deviceNum] = df_dw['octets_passed'].max()
+    logger.debug('peakByesPerUser["up"] = '+str(peakBytesPerUser['up']))
+
+    # 1. Peak Per Day CDF
     # group by direction and date; find indices for peak octets_passed per day
     # transform(max) returns the actual max value per group for each index
     # compare with original table to get only indices for which max was true
@@ -134,17 +124,17 @@ def update_cdf(filename, folder):
     peakPerDay_up = df_up[idx2].groupby("date").first()
 
     # save in huge list over which CDF or hist will be plotted
-    peakHist['dw'].extend( list(peakPerDay_dw['octets_passed']) )
-    peakHist['up'].extend( list(peakPerDay_up['octets_passed']) )
-    #peakHist['dw'] += Counter( list(peakPerDay_dw['octets_passed']) )
-    #peakHist['up'] += Counter( list(peakPerDay_up['octets_passed']) )
+    #peakHist['dw'].extend( list(peakPerDay_dw['octets_passed']) )
+    #peakHist['up'].extend( list(peakPerDay_up['octets_passed']) )
+    peakHist['dw'] += Counter( list(peakPerDay_dw['octets_passed']) )
+    peakHist['up'] += Counter( list(peakPerDay_up['octets_passed']) )
 
     # 2. Drop Zeros
     dropZeros = df_dev[df_dev['octets_passed'] != 0]
-    #noZeroHist['dw'] += Counter( list(dropZeros[dropZeros['service_direction'] == 1]['octets_passed']) )
-    #noZeroHist['up'] += Counter( list(dropZeros[dropZeros['service_direction'] == 2]['octets_passed']) )
-    noZeroHist['dw'].extend( list(dropZeros[dropZeros['service_direction'] == 1]['octets_passed']) )
-    noZeroHist['up'].extend( list(dropZeros[dropZeros['service_direction'] == 2]['octets_passed']) )
+    noZeroHist['dw'] += Counter( list(dropZeros[dropZeros['service_direction'] == 1]['octets_passed']) )
+    noZeroHist['up'] += Counter( list(dropZeros[dropZeros['service_direction'] == 2]['octets_passed']) )
+    #noZeroHist['dw'].extend( list(dropZeros[dropZeros['service_direction'] == 1]['octets_passed']) )
+    #noZeroHist['up'].extend( list(dropZeros[dropZeros['service_direction'] == 2]['octets_passed']) )
     return
 
 
@@ -160,25 +150,41 @@ def save_hists(folder):
         if (log_counter % DEBUG_LOG_COUNTER_DISPLAY == 0): logger.debug("Counter " + str(log_counter) + ": " + filename)
 
     logger.info("Done updating CDF variables. Now save hists in pkl")
+    # save peakBytes per User pkl
+    folder = datasetname.split('.')[0]
+    if not os.path.exists(OUTPUTPATH+folder):
+        os.makedirs(OUTPUTPATH+folder+"/")
+    f = open(OUTPUTPATH+folder+"/"+'peakBytesPerUser.pkl', 'w')
+    pkl.dump(peakBytesPerUser, f)
+    f.close()
+    logger.info("Pickled peakBytesPerUser for " + folder)
     # pickle hist series data
-    pkl.dump(noZeroHist, open(OUTPUTPATH+folder+"/"+'noZeroCounter.pkl', 'w'))
     pkl.dump(peakHist, open(OUTPUTPATH+folder+"/"+'peakCounter.pkl', 'w'))
-    logger.info("Done saving hists in pkl")
+    logger.info("Pickled peakHist for " + folder)
+    pkl.dump(noZeroHist, open(OUTPUTPATH+folder+"/"+'noZeroCounter.pkl', 'w'))
+    logger.info("Pickled noZeroHist for " + folder)
     return
 
 
 if __name__ == "__main__":
 
+    CREATE_CSV = 1
+    ANALYZE = 1
+
     #for datasetname in ['test-data-set.dat', 'test-control-set.dat']:
     datasets = [x+'.dat' for x in [DATASET, CONTROLSET]]
+
     for datasetname in datasets:
         folder = datasetname.split('.')[0]
-        create_csv(datasetname)
+        if (CREATE_CSV==1):
+            create_csv(datasetname)
 
-        # For each data set
-        noZeroHist = {'up':[], 'dw':[]}
-        peakHist = {'up':[], 'dw':[]}
-        log_counter = 0
+        if (ANALYZE==1):
+            # For each data set
+            noZeroHist = {'up':Counter([]), 'dw':Counter([])}
+            peakHist = {'up':Counter([]), 'dw':Counter([])}
+            peakBytesPerUser = {'up':defaultdict(int), 'dw':defaultdict(int)}
+            log_counter = 0
 
-        # save pkls nonzero and peak
-        save_hists(folder)
+            # save pkls nonzero and peak
+            save_hists(folder)
--- plotter.py
+++ plotter.py
@@ -26,8 +26,8 @@ head -300 control1.dat > test-control-set.dat
 tail -300 control1.dat >> test-control-set.dat
 '''
 
-DATASET='250-test'
-CONTROLSET='control1'
+DATASET='test-data-set'#'250-test'
+CONTROLSET='test-control-set'#'control1'
 
 keys = ['Device_number',
          'end_time',
@@ -56,17 +56,33 @@ def read_dataframe(folder, filename):
     header_row = ['device_number', 'end_time', 'cmts_inet', 'direction', 'bytes']
     return pd.read_csv(PROCESSEDPATH + folder + '/' + filename, names=header_row)
 
-
 # CDF plotter
-def plotCDF(data, ax, c='blue'):
+def plotCDF_list(data, ax, c='blue'):
     '''
     data = list for which we need to plot cdf
     ax = axis
     '''
     sorted_data=np.sort( data )
-    yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    #logger.debug(sorted_data)
+    #yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    yvals=np.cumsum(sorted_data)
+    #logger.debug(yvals)
     ax.plot( sorted_data, yvals, color=c )
     return
+
+def plotCDF_counter(counter, ax, c='blue'):
+    '''
+    data = Counter object, not in increasing order
+    ax = axis
+    '''
+    sorted_keys=sorted( counter )
+    #log.debug(sorted_data)
+    #yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    yvals=np.cumsum([counter[k] for k in sorted_keys])
+    #log.debug(yvals)
+    ax.plot( sorted_keys, yvals, color=c, marker='o' )
+    return
+
 
 if __name__ == "__main__":
 
@@ -82,35 +98,44 @@ if __name__ == "__main__":
     logger.info("Plotting...")
     # plot double subplot CDFs for upstream and downstream
     # peak usage per user over all time
-    fig0, ax0 = plt.subplots(2, 1)
+    fig0, ax0 = plt.subplots(2, 1, sharex=True)
 
-    plotCDF(peakUsage['up'], ax0[0])
-    plotCDF(peakUsage_control['up'], ax0[0], 'g')
+    plotCDF_list(peakUsage['up'].values(), ax0[0])
+    plotCDF_list(peakUsage_control['up'].values(), ax0[0], 'g')
 
-    plotCDF(peakUsage['dw'], ax0[1])
-    plotCDF(peakUsage_control['dw'], ax0[1], 'g')
+    plotCDF_list(peakUsage['dw'].values(), ax0[1])
+    plotCDF_list(peakUsage_control['dw'].values(), ax0[1], 'g')
 
-    fig1.savefig(OUTPUTPATH + "peakBytesPerUser")
+    ax0[1].set_xlabel('Peak Bytes per Device Number [Bytes]')
+    ax0[0].set_ylabel('CDF')
+    ax0[1].set_ylabel('CDF')
+    fig0.savefig(OUTPUTPATH + "peakBytesPerDeviceCDF")
 
     # No Zeros CDF
     fig1, ax1 = plt.subplots(2, 1)
 
-    plotCDF(noZeroHist['up'], ax1[0])
-    plotCDF(noZeroHist_control['up'], ax1[0], 'g')
+    plotCDF_counter(noZeroHist['up'], ax1[0])
+    plotCDF_counter(noZeroHist_control['up'], ax1[0], 'g')
 
-    plotCDF(noZeroHist['dw'], ax1[1])
-    plotCDF(noZeroHist_control['dw'], ax1[1], 'g')
+    plotCDF_counter(noZeroHist['dw'], ax1[1])
+    plotCDF_counter(noZeroHist_control['dw'], ax1[1], 'g')
 
+    ax1[1].set_xlabel('Non Zero Bytes per Device time-slot [Bytes]')
+    ax1[0].set_ylabel('CDF')
+    ax1[1].set_ylabel('CDF')
     fig1.savefig(OUTPUTPATH + "noZeroCDF")
     #fig1.show()
 
     fig2, ax2 = plt.subplots(2, 1)
 
-    plotCDF(peakHist['up'], ax2[0])
-    plotCDF(peakHist_control['up'], ax2[0], 'g')
+    plotCDF_counter(peakHist['up'], ax2[0])
+    plotCDF_counter(peakHist_control['up'], ax2[0], 'g')
 
-    plotCDF(peakHist['dw'], ax2[1])
-    plotCDF(peakHist_control['dw'], ax2[1], 'g')
+    plotCDF_counter(peakHist['dw'], ax2[1])
+    plotCDF_counter(peakHist_control['dw'], ax2[1], 'g')
 
-    fig2.savefig(OUTPUTPATH + "peakCDF")
+    ax2[1].set_xlabel('Peak Bytes per Device per Day [Bytes]')
+    ax2[0].set_ylabel('CDF')
+    ax2[1].set_ylabel('CDF')
+    fig2.savefig(OUTPUTPATH + "peakBytesPerDayPerDeviceCDF")
     #fig2.show()
